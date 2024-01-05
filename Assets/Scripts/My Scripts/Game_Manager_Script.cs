f5723fb85004166c5eb3cdc26afbc4213ad2c066
martinthorpe <martinthorpeortiz2412@gmail.com>
2024-01-05 13:48:34 +0000

    Trying to find a way to implement the event link after it is set by null in the pickup class

--- Assets/Scripts/My Scripts/Game_Manager_Script.cs
+++ Assets/Scripts/My Scripts/Game_Manager_Script.cs
@@ -6,24 +6,25 @@ using UnityEngine;
 public class Game_Manager_Script : MonoBehaviour
 {
     [SerializeField] private GameObject m_GOTimerUI;
-    [SerializeField] private Pickup m_StarList;// = new List<Pickup>();
+    [SerializeField] private GameObject m_GOStarPickup;
+    [SerializeField] private Transform m_tStarSpawn;
+    [SerializeField] private List<Pickup> m_StarList;
     private int m_iStarCount;
 
     private void OnEnable()
     {
-        m_StarList.OnPickUp += PickedUpStar;
+        m_StarList.Add(Instantiate(m_GOStarPickup, m_tStarSpawn).GetComponent<Pickup>());
     }
 
     private void OnDisable()
     {
-        print("o");
-        m_StarList.OnPickUp -= PickedUpStar;
+        m_StarList[0].OnPickUp -= PickedUpStar;
     }
 
     private void Awake()
     {
-        //m_StarList = new List<Pickup>();
-        m_iStarCount = 1;// m_StarList.Count;
+        m_StarList = new List<Pickup>();
+        m_iStarCount = m_StarList.Count;
     }
 
     private void Update()
@@ -33,8 +34,7 @@ public class Game_Manager_Script : MonoBehaviour
 
     private void Start()
     {
-
-        
+        BuildMap();
     }
 
     private void UpdateTimer()
@@ -44,7 +44,9 @@ public class Game_Manager_Script : MonoBehaviour
 
     private void BuildMap()
     {
-
+        //m_StarList.Add(Instantiate(m_GOStarPickup, m_tStarSpawn).GetComponent<Pickup>());
+        m_StarList[0].OnPickUp += PickedUpStar;
+        print("added");
     }
 
     private void SpawnPlayer()
