8511477a1607bfffb5d0c6119995140e1ac8e56b
martinthorpe <martinthorpeortiz2412@gmail.com>
2024-01-05 14:36:34 +0000

    I have fixed the event firing in the game manager

--- Assets/Scripts/My Scripts/Game_Manager_Script.cs
+++ Assets/Scripts/My Scripts/Game_Manager_Script.cs
@@ -9,11 +9,12 @@ public class Game_Manager_Script : MonoBehaviour
     [SerializeField] private GameObject m_GOStarPickup;
     [SerializeField] private Transform m_tStarSpawn;
     [SerializeField] private List<Pickup> m_StarList;
+    private bool m_bHasSetUpLinks;
     private int m_iStarCount;
 
     private void OnEnable()
     {
-        m_StarList.Add(Instantiate(m_GOStarPickup, m_tStarSpawn).GetComponent<Pickup>());
+        //m_StarList.Add(Instantiate(m_GOStarPickup, m_tStarSpawn).GetComponent<Pickup>());
     }
 
     private void OnDisable()
@@ -24,17 +25,22 @@ public class Game_Manager_Script : MonoBehaviour
     private void Awake()
     {
         m_StarList = new List<Pickup>();
-        m_iStarCount = m_StarList.Count;
+        m_bHasSetUpLinks = false;
     }
 
     private void Update()
     {
-
+        if (!m_bHasSetUpLinks)
+        {
+            SetUpLinks();
+            m_bHasSetUpLinks = true;
+        }
     }
 
     private void Start()
     {
         BuildMap();
+        m_iStarCount = m_StarList.Count;
     }
 
     private void UpdateTimer()
@@ -42,11 +48,14 @@ public class Game_Manager_Script : MonoBehaviour
 
     }
 
+    private void SetUpLinks()
+    {
+        m_StarList[0].OnPickUp += PickedUpStar;
+    }
+
     private void BuildMap()
     {
-        //m_StarList.Add(Instantiate(m_GOStarPickup, m_tStarSpawn).GetComponent<Pickup>());
-        m_StarList[0].OnPickUp += PickedUpStar;
-        print("added");
+        m_StarList.Add(Instantiate(m_GOStarPickup, m_tStarSpawn).GetComponent<Pickup>());
     }
 
     private void SpawnPlayer()
@@ -58,6 +67,7 @@ public class Game_Manager_Script : MonoBehaviour
     {
         m_iStarCount--;
         print(m_iStarCount);
+        print(up.ScoreValue);
     }
 
     public void EnteredFinishedArea()
